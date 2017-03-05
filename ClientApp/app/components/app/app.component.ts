7bafe9fdc05d6f0c1ab02f341cefbffff7aeeb0c
Kazuki Ota <k_ota28@hotmail.com>
2017-03-05 22:17:13 +0900

    Exposing heroes

--- ClientApp/app/components/app/app.component.ts
+++ ClientApp/app/components/app/app.component.ts
@@ -1,6 +1,20 @@
 import { Component } from '@angular/core';
 import { Hero } from "../../models/Hero";
 
+const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
+
+
 @Component({
     selector: 'app',
     templateUrl: './app.component.html',
@@ -8,6 +22,9 @@ import { Hero } from "../../models/Hero";
 })
 export class AppComponent {
     title = 'Tour of Heroes';
+
+    heroes = HEROES;
+
     hero: Hero = {
         id: 1,
         name: 'Windstorm',
