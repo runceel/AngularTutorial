d4a44979af4bdebe783b03ceab1762ca960bc85b
Kazuki Ota <k_ota28@hotmail.com>
2017-03-05 22:12:35 +0900

    Editing our hero(tow way binding)

--- ClientApp/app/app.module.ts
+++ ClientApp/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { UniversalModule } from 'angular2-universal';
-import { AppComponent } from './components/app/app.component'
+import { AppComponent } from './components/app/app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
     bootstrap: [ AppComponent ],
@@ -10,6 +11,7 @@ import { AppComponent } from './components/app/app.component'
     ],
     imports: [
         UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
+        FormsModule,
     ]
 })
 export class AppModule {
