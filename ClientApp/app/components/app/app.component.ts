5caf5b5ef9b56ecd5b531f7885cb87b76a010806
Kazuki Ota <k_ota28@hotmail.com>
2017-03-05 22:26:24 +0900

    Expose the selected hero and hide the empty detail with ngif

--- ClientApp/app/components/app/app.component.ts
+++ ClientApp/app/components/app/app.component.ts
@@ -25,8 +25,9 @@ export class AppComponent {
 
     heroes = HEROES;
 
-    hero: Hero = {
-        id: 1,
-        name: 'Windstorm',
-    };
+    selectedHero: Hero;
+
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
 }
