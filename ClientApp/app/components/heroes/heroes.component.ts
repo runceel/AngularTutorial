ac63459f7b51c3743b3ea59377b3cdfc689f2b8f
Kazuki Ota <kaota@microsoft.com>
2017-03-06 20:46:13 +0900

    create heroes

--- ClientApp/app/components/heroes/heroes.component.ts
+++ ClientApp/app/components/heroes/heroes.component.ts
@@ -1,11 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { HeroService } from "../../services/hero.service";
+import { Hero } from "../../models/hero";
 
 @Component({
+    moduleId: module.id + '',
     selector: 'my-heroes',
     templateUrl: './heroes.component.html'
 })
 export class HeroesComponent implements OnInit {
-    constructor() { }
 
-    ngOnInit() { }
+    heroes: Hero[];
+
+    selectedHero: Hero;
+
+    constructor(
+        private router: Router,
+        private heroService: HeroService,
+    ) { }
+
+    ngOnInit() { 
+        this.getHeroes();
+    }
+
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
+
+    gotoDetail(): void {
+        this.router.navigate(['/detail', this.selectedHero.id]);
+    }
+
+    private async getHeroes() {
+        this.heroes = await this.heroService.getHeroes();
+    }
 }
