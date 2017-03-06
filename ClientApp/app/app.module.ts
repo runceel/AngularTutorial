5e673828f71e64908a975eb822a501ebe8cc9e0b
Kazuki Ota <kaota@microsoft.com>
2017-03-06 11:23:27 +0900

    multiple components

--- ClientApp/app/app.module.ts
+++ ClientApp/app/app.module.ts
@@ -3,11 +3,13 @@ import { RouterModule } from '@angular/router';
 import { UniversalModule } from 'angular2-universal';
 import { AppComponent } from './components/app/app.component';
 import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from "./components/hero-detail/hero-detail.component";
 
 @NgModule({
     bootstrap: [ AppComponent ],
     declarations: [
         AppComponent,
+        HeroDetailComponent,
     ],
     imports: [
         UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
--- ClientApp/app/components/app/app.component.ts
+++ ClientApp/app/components/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Hero } from "../../models/Hero";
+import { Hero } from "../../models/hero";
 
 const HEROES: Hero[] = [
     { id: 11, name: 'Mr. Nice' },
