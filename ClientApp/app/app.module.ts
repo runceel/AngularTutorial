4fdbf23caed774e735a64eaba720ce2303b81146
Kazuki Ota <kaota@microsoft.com>
2017-03-06 12:29:49 +0900

    add router

--- ClientApp/app/app.module.ts
+++ ClientApp/app/app.module.ts
@@ -4,16 +4,35 @@ import { UniversalModule } from 'angular2-universal';
 import { AppComponent } from './components/app/app.component';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from "./components/hero-detail/hero-detail.component";
+import { HeroesComponent } from "./components/heroes/heroes.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
 
 @NgModule({
     bootstrap: [ AppComponent ],
     declarations: [
         AppComponent,
         HeroDetailComponent,
+        HeroesComponent,
+        DashboardComponent,
     ],
     imports: [
         UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
         FormsModule,
+        RouterModule.forRoot([
+            {
+                path: 'heroes',
+                component: HeroesComponent,
+            },
+            {
+                path: 'dashboard',
+                component: DashboardComponent,
+            },
+            {
+                path: '',
+                redirectTo: '/dashboard',
+                pathMatch: 'full',
+            }
+        ]),
     ]
 })
 export class AppModule {
--- ClientApp/app/components/app/app.component.ts
+++ ClientApp/app/components/app/app.component.ts
@@ -7,26 +7,10 @@ import { HeroService } from "../../services/hero.service";
     selector: 'app',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    providers: [HeroService],
 })
 export class AppComponent implements OnInit {
-    constructor(private heroService: HeroService) {}
-
     title = 'Tour of Heroes';
-
-    heroes: Hero[];
-
-    selectedHero: Hero;
-
+    
     ngOnInit(): void {
-        this.getHeroes();
-    }
-
-    onSelect(hero: Hero): void {
-        this.selectedHero = hero;
-    }
-
-    private async getHeroes() {
-        this.heroes = await this.heroService.getHeroes();
     }
 }
