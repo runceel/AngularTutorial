03b9487a75a650dfff509e1781c1538712c294f9
Kazuki Ota <kaota@microsoft.com>
2017-03-06 12:15:00 +0900

    add HeroService

--- ClientApp/app/components/app/app.component.ts
+++ ClientApp/app/components/app/app.component.ts
@@ -1,33 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from "../../models/hero";
-
-const HEROES: Hero[] = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-];
+import { HeroService } from "../../services/hero.service";
 
 
 @Component({
     selector: 'app',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.css'],
+    providers: [HeroService],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    constructor(private heroService: HeroService) {}
+
     title = 'Tour of Heroes';
 
-    heroes = HEROES;
+    heroes: Hero[];
 
     selectedHero: Hero;
 
+    ngOnInit(): void {
+        this.getHeroes();
+    }
+
     onSelect(hero: Hero): void {
         this.selectedHero = hero;
+    }
+
+    private async getHeroes() {
+        this.heroes = await this.heroService.getHeroes();
     }
 }
